3a599c6b8d5ad8941ea9d648da7654c20ac18900
LeonidGHA <stowk1992@gmail.com>
2022-08-26 16:03:25 +0300

    add full task

--- src/components/ItemGallery/ItemsGallery.jsx
+++ src/components/ItemGallery/ItemsGallery.jsx
@@ -1,57 +1,88 @@
-import { Component } from "react";
+import { useState, useEffect, useCallback } from "react";
 
-class ItemsGallery extends Component {
-  state = {
-    items: [],
-    page: 1,
-  };
+const ItemsGallery = () => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
 
-  componentDidMount() {
-    this.fetchRespose();
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { page } = this.state;
-    if (page !== prevState.page) {
-      this.fetchRespose();
-    }
-  }
-
-  fetchRespose() {
-    const { page } = this.state;
+  const fetchRespose = useCallback(() => {
     fetch(`https://jsonplaceholder.typicode.com/users/${page}/albums`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-
-        this.setState((prevState) => ({
-          items: page > 1 ? [...prevState.items, ...data] : data,
-        }));
+        // console.log(data);
+        setItems((prevItems) => (page > 1 ? [...prevItems, ...data] : data));
       })
       .catch(console.error);
-  }
+  }, [page]);
 
-  render() {
-    const { items } = this.state;
+  useEffect(() => {
+    fetchRespose();
+  }, [fetchRespose]);
 
-    return (
-      <div>
-        <ul>
-          {items.map(({ id, title }) => (
-            <li key={id}>{title}</li>
-          ))}
-        </ul>
-        <button
-          type="button"
-          onClick={() =>
-            this.setState((prevState) => ({ page: prevState.page + 1 }))
-          }
-        >
-          Load more
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ul>
+        {items.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={() => setPage(page + 1)}>
+        Load more
+      </button>
+    </div>
+  );
+};
 
 export default ItemsGallery;
+// class ItemsGallery extends Component {
+//   state = {
+//     items: [],
+//     page: 1,
+//   };
+
+//   componentDidMount() {
+//     this.fetchRespose();
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     const { page } = this.state;
+//     if (page !== prevState.page) {
+//       this.fetchRespose();
+//     }
+//   }
+
+//   fetchRespose() {
+//     const { page } = this.state;
+//     fetch(`https://jsonplaceholder.typicode.com/users/${page}/albums`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+
+//         this.setState((prevState) => ({
+//           items: page > 1 ? [...prevState.items, ...data] : data,
+//         }));
+//       })
+//       .catch(console.error);
+//   }
+
+//   render() {
+//     const { items } = this.state;
+
+//     return (
+//       <div>
+//         <ul>
+//           {items.map(({ id, title }) => (
+//             <li key={id}>{title}</li>
+//           ))}
+//         </ul>
+//         <button
+//           type="button"
+//           onClick={() =>
+//             this.setState((prevState) => ({ page: prevState.page + 1 }))
+//           }
+//         >
+//           Load more
+//         </button>
+//       </div>
+//     );
+//   }
+// }
